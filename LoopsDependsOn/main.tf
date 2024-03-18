31a39952c02990651014aa6a573b0f2ec3c14cf0
Fabio Brito Pinto <fabiobritopinto@gmail.com>
2024-03-18 10:56:21 -0300

    Loops e For_eatch

--- LoopsDependsOn/main.tf
+++ LoopsDependsOn/main.tf
@@ -13,21 +13,35 @@ provider "azurerm" {
   alias = "VSCodeProfessional"
 }
 
-resource "azurerm_resource_group" "rg_loop" {
+resource "azurerm_resource_group" "rg_terraform" {
   count    = 2
-  name     = "RgLoop-${count.index}"
-  provider = azurerm.VSCodeProfessional
   location = "eastus"
+  name     = "rg-terraform-${count.index}"
+  provider = azurerm.VSCodeProfessional
+  tags = {
+    data        = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())
+    ambiente    = lower("Homologacao")
+    responsavel = upper("Fabio Brito")
+    tecnologia  = title("hashicorp terraform")
+  }
 }
 
-resource "azurerm_virtual_network" "vnet" {
+variable "vnetips" {
+  type    = list(any)
+  default = ["192.168.100.0/24"]
+
+}
+resource "azurerm_virtual_network" "vnet_qas" {
   provider            = azurerm.VSCodeProfessional
-  name                = "vnet"
-  resource_group_name = "RgLoop-1"
+  name                = "vnet-qas"
+  resource_group_name = "rg-terraform-1"
   location            = "eastus"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = length(var.vnetips) == 0 ? ["10.0.0.0/16", "192.168.0.0/16"] : var.vnetips
+  depends_on = [
+    azurerm_resource_group.rg_terraform
+  ]
 }
 
-output "vnets_ips" {
-  value = length(azurerm_virtual_network.vnet)
+output "vnet_num_address_space" {
+  value = length("${azurerm_virtual_network.vnet_qas.address_space}")
 }
---
+++ LoopsDependsOn/nsg.tf
@@ -0,0 +1,51 @@
+provider "azurerm" {
+  subscription_id = "ff882453-92d9-4cdd-a627-966b0727aeec"
+  features {}
+}
+
+resource "azurerm_resource_group" "vnet_rg" {
+  provider = azurerm.VSCodeProfessional
+  name     = "vnet_rg"
+  location = "eastus"
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  provider            = azurerm.VSCodeProfessional
+  name                = "nsg"
+  resource_group_name = azurerm_resource_group.vnet_rg.name
+  location            = azurerm_resource_group.vnet_rg.location
+  depends_on = [
+    azurerm_resource_group.vnet_rg
+  ]
+}
+
+variable "portas_input" {
+  type        = map(any)
+  description = "Lista de portas Liberadas para Entrada"
+  default = {
+    110 = 80
+    120 = 443
+    130 = 22
+    140 = 3389
+  }
+}
+
+
+resource "azurerm_network_security_rule" "regras_inbound" {
+  for_each                    = var.portas_input
+  resource_group_name         = azurerm_resource_group.vnet_rg.name
+  name                        = "PortaEntrada_${each.value}"
+  priority                    = each.key
+  destination_port_range      = each.value
+  direction                   = "Inbound"
+  access                      = "Allow"
+  source_port_range           = "*"
+  protocol                    = "Tcp"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_group
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_rule
