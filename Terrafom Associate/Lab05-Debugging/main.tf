8032574526b347ffc57afe499fb9131f986bbbf1
fabiobritopinto@gmail.com <fabiobritopinto@gmail.com>
2025-05-17 19:42:02 -0300

    Lab para usar com o debbug

---
+++ Terrafom Associate/Lab05-Debugging/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.26.0"
+    }
+
+    randon = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+
+  }
+}
+
+module "storage_module" {
+  source = "./storage_module"
+}
+
+#Terraform Output Values Block
+output "storage_account_id" {
+  value = module.storage_module.storage_account_id
+}
+output "storage_account_name" {
+  value = module.storage_module.storage_account_name
+}
---
+++ Terrafom Associate/Lab05-Debugging/tarefas.md
@@ -0,0 +1,14 @@
+#### Conceitos Usando Debug de Logs
+## Objetivos do Lab:
+01 - Habilitar o Log
+```bash
+export TF_LOG=TRACE
+```
+02 - Habilitar e configurar o Logging Path
+```bash
+export TF_LOG_PATH="/var/log/terraform/terraform_log.log"
+```
+03 - Desabilitar o Log
+```bash
+export TF_LOG=
+```
